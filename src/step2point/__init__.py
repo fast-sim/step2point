8fe138dadfabbadef4a332778818d608b3ac94dd
Anna Zaborowska <anna.zaborowska@cern.ch>
2026-04-15 15:28:54 +0200

    Add version

--- src/step2point/__init__.py
+++ src/step2point/__init__.py
@@ -1,5 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from step2point.algorithms.identity import IdentityCompression
 from step2point.algorithms.merge_within_cell import MergeWithinCell
 from step2point.core.shower import Shower
 
-__all__ = ["Shower", "IdentityCompression", "MergeWithinCell"]
+try:
+    __version__ = version("step2point")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
+
+__all__ = ["__version__", "Shower", "IdentityCompression", "MergeWithinCell"]
