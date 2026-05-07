002a17bafd60ef99e6ed896fa874fbff76f76738
Saransh Chopra <saransh0701@gmail.com>
2026-05-07 21:46:47 +0200

    fix ruff

--- tests/integration/test_algorithm_cluster_within_cell_agglomerative_strict_regression.py
+++ tests/integration/test_algorithm_cluster_within_cell_agglomerative_strict_regression.py
@@ -14,5 +14,7 @@ pytestmark = pytest.mark.strict_regression
 
 def test_cluster_within_cell_output_matches_reference(tmp_path):
     outdir = run_pipeline(tmp_path, "cluster_within_cell_agglomerative")
-    assert_summary_equals(outdir / "compression_summary_cluster_within_cell_agglomerative.txt", "cluster_within_cell_agglomerative")
+    assert_summary_equals(
+        outdir / "compression_summary_cluster_within_cell_agglomerative.txt", "cluster_within_cell_agglomerative"
+    )
     assert_showers_equal(CLUSTER_WITHIN_CELL_REFERENCE, outdir / "compressed_cluster_within_cell_agglomerative.h5")
