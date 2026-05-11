8b5a661e9ab39e36af5dbfb7e29a12fffdb0e209
Mmozzanica5 <martina.mozzanica@uni-hamburg.de>
2026-05-11 17:37:38 +0200

    file to convert the output of step3point to cc3 format implemented

--- martina_test/minimal_root_reader.py
+++ martina_test/minimal_root_reader.py
@@ -12,9 +12,9 @@ reader = EDM4hepRootReader(
     ),
     # shower_limit=1,
 )
-# i think it does not work properly for ILD because of the even/odd layers in the ECal 
+# i think it does not work properly for ILD because of the even/odd layers in the ECal
 shower = next(reader.iter_showers())
-print([attr for attr in dir(shower) if not attr.startswith('_')])
+print([attr for attr in dir(shower) if not attr.startswith("_")])
 print(shower.n_points, shower.cell_id is not None, shower.t is not None)
 
 print("n_points:", shower.n_points)
--- martina_test/submit_job.sh
+++ martina_test/submit_job.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+# shellcheck source=/dev/null
 source /cvmfs/sw.hsf.org/key4hep/setup.sh
+
 cd /eos/user/m/mamozzan/step2point/ || exit
+
+# shellcheck source=/dev/null
 source .venv-key4hep/bin/activate
 
 # geometry file in $k4geo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml
-# for loop on seed 
+# for loop on seed
 export algo=merge_within_regular_subcell #[identity, merge_within_cell, merge_within_regular_subcell, hdbscan]
 
 extra_args="merge_within_regular_subcell"
 
+# shellcheck source=/dev/null
 if [[ "$algo" == "merge_within_regular_subcell" ]]; then
-    extra_args="--compact-xml $k4geo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml \
+    extra_args="--compact-xml k4geo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml \
                 --collection-name ECalBarrelCollection \
                 --grid-x 5 \
                 --grid-y 5 \
@@ -20,19 +25,23 @@ fi
 
 for seed in {0..0}; do
     python examples/run_step2point_pipeline.py \
-      --input /eos/user/m/mamozzan/photons_root/p22_th45-135_ph79-109_en5-130_seed${seed}_ip.edm4hep.root \
-      --algorithm $algo \
-      --output outputs/pipeline_ECalBarrelCollection_${algo}_${seed} \
+      --input /eos/user/m/mamozzan/photons_root/p22_th45-135_ph79-109_en5-130_seed"${seed}"_ip.edm4hep.root \
+      --algorithm "$algo" \
+      --output outputs/pipeline_ECalBarrelCollection_"${algo}"_"${seed}" \
       --collections ECalBarrelSiHitsEven ECalBarrelSiHitsOdd ECalEndcapSiHitsEven ECalEndcapSiHitsOdd HcalBarrelRegCollection HcalEndcapRingCollection \
-      $extra_args
+      "$extra_args"
 done
 
 # testing
 export seed=0
 export algo=identity
 python examples/generate_validation_plots.py \
-  --input /eos/user/m/mamozzan/photons_root/p22_th45-135_ph79-109_en5-130_seed${seed}_ip.edm4hep.root \
-  --algorithm ${algo} \
-  --outdir outputs/plots_${algo}_${seed} \
+  --input /eos/user/m/mamozzan/photons_root/p22_th45-135_ph79-109_en5-130_seed"${seed}"_ip.edm4hep.root \
+  --algorithm "${algo}" \
+  --outdir outputs/plots_"${algo}"_"${seed}" \
   --collections ECalBarrelSiHitsEven ECalBarrelSiHitsOdd ECalEndcapSiHitsEven ECalEndcapSiHitsOdd HcalBarrelRegCollection HcalEndcapRingCollection
 
+# convert to cc format
+for seed in {1..10}; do
+    python martina_test/convert_to_cc3_format.py outputs/pipeline_identity/pipeline_identity_"${seed}"/compressed_identity.h5
+done
